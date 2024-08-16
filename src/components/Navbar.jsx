45743f01531dafc0c6b6a87922902703c592cbab
Phạm Hoàng Tiến <87059330+hoangtien2911@users.noreply.github.com>
2024-08-17 04:57:40 +0700

    [TienPH] Update hover

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
                 <div className="flex flex-col justify-center items-center p-4 text-white">
                     <a
                         href="#home"
-                        className={`vertical-text text-base uppercase ${activeMenu === "home" ? "border-l-[2.5px] border-yellow" : ""} hover:text-primaryTextColor`}
+                        className={`vertical-text text-base uppercase ${activeMenu === "home" ? "border-l-[2.5px] border-yellow" : ""} hover:border-l-[2.5px] hover:border-yellow`}
                         onClick={() => handleMenuClick("home")}
                     >
                         Điện Biên Phủ
@@ -49,28 +49,28 @@ const Navbar = () => {
                 <nav className="flex flex-col items-center gap-6 text-white p-4">
                     <a
                         href="#context"
-                        className={`vertical-text text-sm uppercase ${activeMenu === "context" ? "border-l-[2.5px] border-yellow" : ""} hover:text-primaryTextColor`}
+                        className={`vertical-text text-sm uppercase ${activeMenu === "context" ? "border-l-[2.5px] border-yellow" : ""} hover:border-l-[2.5px] hover:border-yellow`}
                         onClick={() => handleMenuClick("context")}
                     >
                         Bối cảnh
                     </a>
                     <a
                         href="#battle"
-                        className={`vertical-text text-sm uppercase ${activeMenu === "battle" ? "border-l-[2.5px] border-yellow" : ""} hover:text-primaryTextColor`}
+                        className={`vertical-text text-sm uppercase ${activeMenu === "battle" ? "border-l-[2.5px] border-yellow" : ""} hover:border-l-[2.5px] hover:border-yellow`}
                         onClick={() => handleMenuClick("battle")}
                     >
                         Các trận đánh
                     </a>
                     <a
                         href="#hero"
-                        className={`vertical-text text-sm uppercase ${activeMenu === "hero" ? "border-l-[2.5px] border-yellow" : ""} hover:text-primaryTextColor`}
+                        className={`vertical-text text-sm uppercase ${activeMenu === "hero" ? "border-l-[2.5px] border-yellow" : ""} hover:border-l-[2.5px] hover:border-yellow`}
                         onClick={() => handleMenuClick("hero")}
                     >
                         Các vị anh hùng
                     </a>
                     <a
                         href="#conclusion"
-                        className={`vertical-text text-sm uppercase ${activeMenu === "conclusion" ? "border-l-[2.5px] border-yellow" : ""} hover:text-primaryTextColor`}
+                        className={`vertical-text text-sm uppercase ${activeMenu === "conclusion" ? "border-l-[2.5px] border-yellow" : ""} hover:border-l-[2.5px] hover:border-yellow`}
                         onClick={() => handleMenuClick("conclusion")}
                     >
                         Tổng kết
